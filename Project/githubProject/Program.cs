7b1802c2b3ce068943504df8ef95a1d7d2144caf
Leo Yen <yenthuan@gmail.com>
2019-04-18 09:44:17 +0000

    test

--- Project/githubProject/Program.cs
+++ Project/githubProject/Program.cs
@@ -15,9 +15,9 @@ namespace githubProject
             IObserver<int> handler = null;
             IDisposable subscription = source.Subscribe(handler);
             //Observable.Range(1,5);//creates an observable sequence of 5 integers, starting from 1
-            IDisposable subscribe = source.Subscribe(x => Console.WriteLine("onNext: {0}", x),
-                                                    ex => Console.WriteLine("onError: {0}", ex),
-                                                    () => Console.WriteLine("onCompleted"));
+            // IDisposable subscribe = source.Subscribe(x => Console.WriteLine("onNext: {0}", x),
+            //                                         ex => Console.WriteLine("onError: {0}", ex),
+            //                                         () => Console.WriteLine("onCompleted"));
 
             Console.WriteLine("Press ENTER to unsubscribe...");
             Console.ReadLine();
