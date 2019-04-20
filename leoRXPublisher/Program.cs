359e1893b9427f6985588fdda6b51b875305d366
Leo Yen <yenthuan@gmail.com>
2019-04-20 13:57:12 +0700

    add while

--- leoRXPublisher/Program.cs
+++ leoRXPublisher/Program.cs
@@ -9,11 +9,19 @@ namespace leoRXPublisher
 
         static void Main(string[] args)
         {
-            
-            var publisher = new PublisherNetMq<int>(V);
-            publisher.OnNext(49); // Sends 42.
-            Console.ReadLine();    
 
+            // var publisher = new PublisherNetMq<int>("tcp://127.0.0.1:56001");
+            // publisher.OnNext(48); // Sends 42.
+            // Console.ReadLine();  
+
+            var publisher = new PublisherNetMq<string>("tcp://127.0.0.1:56000");
+            while (true)
+            {
+                Console.WriteLine("enter the message to send via network");
+                string a = Console.ReadLine();
+
+                publisher.OnNext(a); // Sends 42.    
+            }
         }
     }
 }
--- leoRXSubscriber/Program.cs
+++ leoRXSubscriber/Program.cs
@@ -6,11 +6,12 @@ namespace leoRXSubscriber
     {
         static void Main(string[] args)
         {
-            var subscriber1 = new SubscriberNetMq<int>("tcp://127.0.0.1:56000");
+           // Application 1 (subscriber)
+            var subscriber1 = new SubscriberNetMq<string>("tcp://127.0.0.1:56000");
             subscriber1.Subscribe(message =>
             {
                 Console.WriteLine("I am subscriber and I received message from publisher: {0}",message); // Prints "42".
-            });   
+            });            
 
             Console.ReadLine();
         }
