c9f66988b47a0fab97e7d675cd2404e6523e8e75
Leo Yen <yenthuan@gmail.com>
2019-06-03 01:05:44 +0700

    remove the routeprefix
    c.RoutePrefix = string.Empty;

--- dotnet-core-api-master/Startup.cs
+++ dotnet-core-api-master/Startup.cs
@@ -29,7 +29,6 @@ namespace TodoApi
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                c.RoutePrefix = string.Empty;
             });
 
             app.UseDefaultFiles();
