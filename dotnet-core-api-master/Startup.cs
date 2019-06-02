009eb1af912d699979967c609dce671ce60d39a1
Leo Yen <yenthuan@gmail.com>
2019-06-03 00:58:28 +0700

    Update Startup.cs

--- dotnet-core-api-master/Startup.cs
+++ dotnet-core-api-master/Startup.cs
@@ -29,6 +29,7 @@ namespace TodoApi
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
             });
 
             app.UseDefaultFiles();
